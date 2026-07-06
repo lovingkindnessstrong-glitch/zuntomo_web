c7d79bffb9792f002453e36ff782494144711b35
niku <loving.kindness.strong@gmail.com>
2026-07-06 10:52:02 +0900

    細かい修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import remarkBreaks from 'remark-breaks'; 
+import remarkBreaks from 'remark-breaks';
+
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://zuntomo.com', 
+  site: 'https://zuntomo.com',
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   markdown: {                      
     remarkPlugins: [remarkBreaks], 
-  }                                
+  },
+
+  integrations: [sitemap()]
 });
